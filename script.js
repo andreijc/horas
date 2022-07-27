24dd1340aba6ddbf49a0637af300d05dd2abf443
eunaotenhocriatividade <andreicruz1203@gmail.com>
2022-07-27 18:33:22 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+var agora = new Date()
+var hora = agora.getHours()
+var img = document.getElementById('img')
+var msg = document.querySelector('#msg')
+var corpo = document.querySelector('.corpo')
+
+hora = 1
+
+
+
+if ( 24 > hora && hora < 12) {
+        img.style.backgroundImage = 'url(estilos/imagens/manha.jpg)'
+        msg.innerHTML = '<p>Agora é de manhã</p>' 
+        corpo.style.backgroundImage= 'var(--manha)'
+} else if (18 > hora && hora  >= 12) {
+        img.style.backgroundImage = 'url(estilos/imagens/tarde.jpg)'
+        msg.innerHTML = '<p>Agora é de tarde</p>'
+        corpo.style.backgroundImage = 'var(--tarde)'
+} else if ( 18 <= hora ) {
+        img.style.backgroundImage = 'url(estilos/imagens/noite.jpg)'
+        msg.innerHTML = '<p>Agora é de noite</p>'
+        corpo.style.backgroundImage = 'var(--noite)'
+}
